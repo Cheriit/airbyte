146d41ca399729748a4a9228bbbb0e1eb13978fd
Szczepaniak-M <m.szczepaniak99@gmail.com>
2022-04-02 20:10:51 +0200

    Add ApacheArrowDestination, ApacheArrowDestinationConfig and ApacheArrowRecordConsumer

--- airbyte-integrations/connectors/destination-apache-arrow/src/main/java/io/airbyte/integrations/destination/apache_arrow/ApacheArrowDestinationConfig.java
+++ airbyte-integrations/connectors/destination-apache-arrow/src/main/java/io/airbyte/integrations/destination/apache_arrow/ApacheArrowDestinationConfig.java
@@ -1,12 +1,48 @@
+/*
+ * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.integrations.destination.apache_arrow;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ApacheArrowDestinationConfig {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(ApacheArrowDestinationConfig.class);
+    private static final String DESTINATION_PATH_FIELD = "destination_path";
+    private final Path destinationPath;
 
-    private ApacheArrowDestinationConfig() {
-        // TODO Add fields to config class
+    private ApacheArrowDestinationConfig(Path destinationPath) {
+        this.destinationPath = destinationPath;
     }
 
-    public static ApacheArrowDestinationConfig getApacheArrowDestinationConfig(final JsonNode config) {
-        return new ApacheArrowDestinationConfig()();
+    public static ApacheArrowDestinationConfig of(final JsonNode config) {
+        Path destinationPath = getDestinationPath(config);
+        return new ApacheArrowDestinationConfig(destinationPath);
+    }
+
+    private static Path getDestinationPath(JsonNode config) {
+        Path destinationPath = Paths.get(config.get(DESTINATION_PATH_FIELD).asText());
+        Preconditions.checkNotNull(destinationPath);
+
+        if (!destinationPath.startsWith("/local")) {
+            destinationPath = Path.of("/local", destinationPath.toString());
+        }
+
+        final Path normalizePath = destinationPath.normalize();
+        if (!normalizePath.startsWith("/local")) {
+            throw new IllegalArgumentException("Destination file should be inside the /local directory");
+        }
+        return destinationPath;
+    }
+
+    public Path getDestinationPath() {
+        return destinationPath;
     }
 }
--- airbyte-integrations/connectors/destination-apache-arrow/src/main/java/io/airbyte/integrations/destination/apache_arrow/ApacheArrowRecordConsumer.java
+++ airbyte-integrations/connectors/destination-apache-arrow/src/main/java/io/airbyte/integrations/destination/apache_arrow/ApacheArrowRecordConsumer.java
@@ -2,17 +2,41 @@
  * Copyright (c) 2021 Airbyte, Inc., all rights reserved.
  */
 
+package io.airbyte.integrations.destination.apache_arrow;
+
+import io.airbyte.integrations.base.FailureTrackingAirbyteMessageConsumer;
+import io.airbyte.protocol.models.AirbyteMessage;
+import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.function.Consumer;
+
 public class ApacheArrowRecordConsumer extends FailureTrackingAirbyteMessageConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApacheArrowRecordConsumer.class);
-
     private final ConfiguredAirbyteCatalog catalog;
 
     public ApacheArrowRecordConsumer(final ApacheArrowDestinationConfig apacheArrowDestinationConfig,
                                      final ConfiguredAirbyteCatalog catalog,
                                      final Consumer<AirbyteMessage> outputRecordCollector) {
-        super(outputRecordCollector);
         this.catalog = catalog;
-        LOGGER.info("initializing consumer.");
+        LOGGER.info("Initializing ApacheArrowRecordConsumer.");
     }
+
+    @Override
+    protected void startTracked() throws Exception {
+
+    }
+
+    @Override
+    protected void acceptTracked(final AirbyteMessage airbyteMessage) throws Exception {
+
+    }
+
+    @Override
+    protected void close(final boolean hasFailed) throws Exception {
+
+    }
+
 }
